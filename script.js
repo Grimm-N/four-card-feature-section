8b024f9fcf6951108ddf61ed07f94f6ab472f72d
Grimm_N <natali.grimm.mim@gmail.com>
2024-09-20 18:55:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+  }
+  
+  function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+  }
+  
+  // Закрытие по клику вне окна
+  window.onclick = function(event) {
+    var modals = document.getElementsByClassName('card__modal');
+    for (let i = 0; i < modals.length; i++) {
+      if (event.target == modals[i]) {
+        modals[i].style.display = "none";
+      }
+    }
+  }
+  
